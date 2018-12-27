215012724a6ade7421f7ab23d6056872e4344ff2
mjlk1 <maclebek@gmail.com>
2018-12-27 23:54:29 +0100

    zmiany ostatnie

--- bullett.cpp
+++ bullett.cpp
@@ -27,68 +27,48 @@ state RHS(const state &r)
 state Euler1(const state &r, const real &h)
 {
 	state sol;
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		sol.push_back(r[i]+h*RHS(r)[i]);
-	}
 	return sol;
 }
 
 state Euler2(const state &r, const real &h)
 {
 	state sol;
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		sol.push_back(r[i]+h*RHS(Euler1(r,0.5*h))[i]);
-	}
 	return sol;
 }
 
 state RK4(const state &r, const real &h)
 {
 
-	state k1,k2,k3,k4,yk1,yk2,yk3,sol;
+	state k1, k2, k3, k4, yk1, yk2, yk3, sol;
 	
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		k1.push_back(h*RHS(r)[i]); //obliczamy k1
-	}
 
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		yk1.push_back(r[i]+0.5*k1[i]); //obliczamy yk1
-	}
 
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		k2.push_back(h*RHS(yk1)[i]); //obliczamy k2
-	}
 
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		yk2.push_back(r[i]+0.5*k2[i]); //obliczamy yk2
-	}
 	
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		k3.push_back(h*RHS(yk2)[i]); //obliczamy k3
-	}
 
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		yk3.push_back(r[i]+k3[i]); //obliczamy yk3
-	}
 
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		k4.push_back(h*RHS(yk3)[i]); //obliczamy k3
-	}
 
 
-	for (int i=0;i<4;++i)
-	{
+	for (int_fast32_t i=0;i<4;++i)
 		sol.push_back(r[i]+(k1[i]+2*k2[i]+2*k3[i]+k4[i])/6); //obliczamy k3
-	}
 
 	return sol;
 }
@@ -97,17 +77,18 @@ state RK4(const state &r, const real &h)
 
 int main()
 {
-	real time, h, hplot;
+	real time;
+	int_fast32_t steps, nplot;
 	 
 	cout << "time=" << '\n';
 	cin >> time;
-	cout << "Dlugosc kroku h=?" << '\n';
-	cin >> h;
-	cout << "Dlugosc kroku do rysowania wykresu hplot=?" << '\n';
-	cin >> hplot;
+	cout << "Liczba krokow=" << '\n';
+	cin >> steps;
+	real h=time/steps;
+	cout << "Dlugosc kroku wynosi" << '\t' << h << '\n';
+	cout << "nplot=" << '\n';
+	cin>>nplot;
 
-	int steps=time/h;
-	int kplot=hplot/h;
 	state r(4);
 	r[0]=0.0;
 	r[1]=0.0;
@@ -121,16 +102,17 @@ int main()
 	vector<state> solution;
 	solution.push_back(r);
 
-	for (int i=1;i<=steps;++i)
+	for (int_fast32_t i=1;i<=steps;++i)
 	{
 		r=RK4(r,h);
 		solution.push_back(r);
 	}
 
-	for (int i=0;i<=steps;i+=kplot)
+	for (int_fast32_t i=0;i<=steps;++i)
 	{
+		if(i % nplot == 0)
 		cout << i*h << '\t' << solution[i][0] << '\t' << solution[i][1] << '\t' << solution[i][2] << '\t' << solution[i][3] << '\n';
 	}
 
-return 0;
+	return 0;
 }
