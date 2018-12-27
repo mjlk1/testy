0437da8cb1c213b5e56291d37d0f99b419426e18
mjlk1 <maclebek@gmail.com>
2018-12-28 00:26:32 +0100

    poprawa stylu cd, setw, duze litery

--- bullett.cpp
+++ bullett.cpp
@@ -6,66 +6,66 @@
 
 using namespace std;
 
-typedef float real;
-typedef vector<real> state; //wektor stanu (x,y,vx,vy) 
+typedef float Real;
+typedef vector<Real> State; //wektor stanu (x,y,vx,vy) 
 
-const real g=9.81;
-const real C=0.001;
-const real mass=1;
-const real n=2;
+const Real g=9.81;
+const Real C=0.001;
+const Real mass=1;
+const Real n=2;
+const int_fast32_t a=10; //Jesli Real to double, wtedy a=18
 
-state RHS(const state &r)
+State rhs(const State &r)
 {
-	state deriv;
+	State deriv;
+	Real l = -C*pow(r[2]*r[2]+r[3]*r[3], (n-1)/2)/mass;
 	deriv.push_back(r[2]);
 	deriv.push_back(r[3]);
-	deriv.push_back(-C*pow(r[2]*r[2]+r[3]*r[3], (n-1)/2)*r[2]/mass);
-	deriv.push_back(-C*pow(r[2]*r[2]+r[3]*r[3], (n-1)/2)*r[3]/mass-g);
+	deriv.push_back(l*r[2]);
+	deriv.push_back(l*r[3]-g);
 	return deriv;
 }
 
-state Euler1(const state &r, const real &h)
+State euler1(const State &r, const Real &h)
 {
-	state sol;
+	State sol;
 	for (int_fast32_t i=0;i<4;++i)
-		sol.push_back(r[i]+h*RHS(r)[i]);
+		sol.push_back(r[i]+h*rhs(r)[i]);
 	return sol;
 }
 
-state Euler2(const state &r, const real &h)
+State euler2(const State &r, const Real &h)
 {
-	state sol;
+	State sol;
 	for (int_fast32_t i=0;i<4;++i)
-		sol.push_back(r[i]+h*RHS(Euler1(r,0.5*h))[i]);
+		sol.push_back(r[i]+h*rhs(euler1(r,0.5*h))[i]);
 	return sol;
 }
 
-state RK4(const state &r, const real &h)
+State rk4(const State &r, const Real &h)
 {
-
-	state k1, k2, k3, k4, yk1, yk2, yk3, sol;
+	State k1, k2, k3, k4, yk1, yk2, yk3, sol;
 	
 	for (int_fast32_t i=0;i<4;++i)
-		k1.push_back(h*RHS(r)[i]); //obliczamy k1
+		k1.push_back(h*rhs(r)[i]); //obliczamy k1
 
 	for (int_fast32_t i=0;i<4;++i)
 		yk1.push_back(r[i]+0.5*k1[i]); //obliczamy yk1
 
 	for (int_fast32_t i=0;i<4;++i)
-		k2.push_back(h*RHS(yk1)[i]); //obliczamy k2
+		k2.push_back(h*rhs(yk1)[i]); //obliczamy k2
 
 	for (int_fast32_t i=0;i<4;++i)
 		yk2.push_back(r[i]+0.5*k2[i]); //obliczamy yk2
 	
 	for (int_fast32_t i=0;i<4;++i)
-		k3.push_back(h*RHS(yk2)[i]); //obliczamy k3
+		k3.push_back(h*rhs(yk2)[i]); //obliczamy k3
 
 	for (int_fast32_t i=0;i<4;++i)
 		yk3.push_back(r[i]+k3[i]); //obliczamy yk3
 
 	for (int_fast32_t i=0;i<4;++i)
-		k4.push_back(h*RHS(yk3)[i]); //obliczamy k3
-
+		k4.push_back(h*rhs(yk3)[i]); //obliczamy k3
 
 	for (int_fast32_t i=0;i<4;++i)
 		sol.push_back(r[i]+(k1[i]+2*k2[i]+2*k3[i]+k4[i])/6); //obliczamy k3
@@ -74,44 +74,43 @@ state RK4(const state &r, const real &h)
 }
 
 
-
 int main()
 {
-	real time;
+	Real time;
 	int_fast32_t steps, nplot;
 	 
 	cout << "time=" << '\n';
 	cin >> time;
 	cout << "Liczba krokow=" << '\n';
 	cin >> steps;
-	real h=time/steps;
+	Real h=time/steps;
 	cout << "Dlugosc kroku wynosi" << '\t' << h << '\n';
 	cout << "nplot=" << '\n';
-	cin>>nplot;
+	cin >> nplot;
 
-	state r(4);
-	r[0]=0.0;
-	r[1]=0.0;
+	State r(4);
+	r[0] = 0.0;
+	r[1] = 0.0;
  
-
-	cout << "vx=?" << '\n';
+ 	cout << "vx=?" << '\n';
 	cin >> r[2];
 	cout << "vy=?" << '\n';
 	cin >> r[3];
 
-	vector<state> solution;
+	vector<State> solution;
 	solution.push_back(r);
 
-	for (int_fast32_t i=1;i<=steps;++i)
+	for (int_fast32_t i=0;i<steps;++i)
 	{
-		r=RK4(r,h);
+		r = rk4(r,h);
 		solution.push_back(r);
 	}
 
 	for (int_fast32_t i=0;i<=steps;++i)
 	{
-		if(i % nplot == 0)
-		cout << i*h << '\t' << solution[i][0] << '\t' << solution[i][1] << '\t' << solution[i][2] << '\t' << solution[i][3] << '\n';
+		if (i%nplot == 0)
+			cout << setw(a) << i*h << setw(a) << solution[i][0] << setw(a) << solution[i][1] << setw(a) << solution[i][2] << setw(a) 
+			<< solution[i][3] << '\n';
 	}
 
 	return 0;
