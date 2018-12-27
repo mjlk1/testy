3bc2160ae9e6d351b3691b14936fa5d971e52c9d
mjlk1 <maclebek@gmail.com>
2018-12-27 22:54:36 +0100

    poprawa stylu

--- bullett.cpp
+++ bullett.cpp
@@ -27,7 +27,7 @@ state RHS(const state &r)
 state Euler1(const state &r, const real &h)
 {
 	state sol;
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		sol.push_back(r[i]+h*RHS(r)[i]);
 	}
@@ -37,7 +37,7 @@ state Euler1(const state &r, const real &h)
 state Euler2(const state &r, const real &h)
 {
 	state sol;
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		sol.push_back(r[i]+h*RHS(Euler1(r,0.5*h))[i]);
 	}
@@ -49,43 +49,43 @@ state RK4(const state &r, const real &h)
 
 	state k1,k2,k3,k4,yk1,yk2,yk3,sol;
 	
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		k1.push_back(h*RHS(r)[i]); //obliczamy k1
 	}
 
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		yk1.push_back(r[i]+0.5*k1[i]); //obliczamy yk1
 	}
 
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		k2.push_back(h*RHS(yk1)[i]); //obliczamy k2
 	}
 
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		yk2.push_back(r[i]+0.5*k2[i]); //obliczamy yk2
 	}
 	
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		k3.push_back(h*RHS(yk2)[i]); //obliczamy k3
 	}
 
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		yk3.push_back(r[i]+k3[i]); //obliczamy yk3
 	}
 
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		k4.push_back(h*RHS(yk3)[i]); //obliczamy k3
 	}
 
 
-	for(int i=0;i<4;++i)
+	for (int i=0;i<4;++i)
 	{
 		sol.push_back(r[i]+(k1[i]+2*k2[i]+2*k3[i]+k4[i])/6); //obliczamy k3
 	}
@@ -97,14 +97,14 @@ state RK4(const state &r, const real &h)
 
 int main()
 {
-	real time,h,hplot;
+	real time, h, hplot;
 	 
-	cout<<"time="<<'\n';
-	cin>>time;
-	cout<<"Dlugosc kroku h=?"<<'\n';
-	cin>>h;
-	cout<<"Dlugosc kroku do rysowania wykresu hplot=?"<<'\n';
-	cin>>hplot;
+	cout << "time=" << '\n';
+	cin >> time;
+	cout << "Dlugosc kroku h=?"<<'\n';
+	cin >> h;
+	cout << "Dlugosc kroku do rysowania wykresu hplot=?"<<'\n';
+	cin >> hplot;
 
 	int steps=time/h;
 	int kplot=hplot/h;
@@ -113,23 +113,23 @@ int main()
 	r[1]=0.0;
  
 
-	cout<<"vx=?"<<'\n';
-	cin>>r[2];
-	cout<<"vy=?"<<'\n';
-	cin>>r[3];
+	cout << "vx=?" << '\n';
+	cin >> r[2];
+	cout << "vy=?" << '\n';
+	cin >> r[3];
 
 	vector<state> solution;
 	solution.push_back(r);
 
-	for(int i=1;i<=steps;++i)
+	for (int i=1;i<=steps;++i)
 	{
 		r=RK4(r,h);
 		solution.push_back(r);
 	}
 
-	for(int i=0;i<=steps;i+=kplot)
+	for (int i=0;i<=steps;i+=kplot)
 	{
-		cout<<i*h<<'\t'<<solution[i][0]<<'\t'<<solution[i][1]<<'\t'<<solution[i][2]<<'\t'<<solution[i][3]<<'\n';
+		cout << i*h << '\t' << solution[i][0] << '\t' << solution[i][1] << '\t' << solution[i][2] << '\t' << solution[i][3] << '\n';
 	}
 
 }
