9e35907a9cdc9a4265edbe96d4960e213e0925e5
mjlk1 <maclebek@gmail.com>
2019-01-02 00:13:33 +0100

    usuniecie mass, nowe testy, poprawa maina

--- source/Derivative.cpp
+++ source/Derivative.cpp
@@ -5,7 +5,7 @@
 State derivative(const State &r, const Parameters &par)
 {
 	State deriv;
-	Real l = -par.C*pow(r[2]*r[2]+r[3]*r[3], (par.n-1)/2)/par.mass;
+	Real l = -par.C*pow(r[2]*r[2]+r[3]*r[3], (par.n-1.0)/2.0);
 	deriv.push_back(r[2]);
 	deriv.push_back(r[3]);
 	deriv.push_back(l*r[2]);
