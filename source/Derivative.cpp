98f9946697070c7e98714ad8665680d666366a60
mjlk1 <maclebek@gmail.com>
2019-01-07 23:47:32 +0100

    zmiana potencjalu, precesja

--- source/Derivative.cpp
+++ source/Derivative.cpp
@@ -5,10 +5,10 @@
 State derivative(const State &r, const Parameters &par)
 {
 	State deriv;
-	Real l = -par.C*pow(r[2]*r[2]+r[3]*r[3], (par.n-1.0)/2.0);
+	Real k = -par.g*(par.C+1.0)/pow((r[0]-1.0)*(r[0]-1.0)+(r[1]-1.0)*(r[1]-1.0),(par.C+3.0)/2.0);
 	deriv.push_back(r[2]);
 	deriv.push_back(r[3]);
-	deriv.push_back(l*r[2]-par.g*(r[0]-1.0)/pow((r[0]-1.0)*(r[0]-1.0)+(r[1]-1.0)*(r[1]-1.0),1.5));
-	deriv.push_back(l*r[3]-par.g*(r[1]-1.0)/pow((r[0]-1.0)*(r[0]-1.0)+(r[1]-1.0)*(r[1]-1.0),1.5));
+	deriv.push_back(k*(r[0]-1.0));
+	deriv.push_back(k*(r[1]-1.0));
 	return deriv;
 }
